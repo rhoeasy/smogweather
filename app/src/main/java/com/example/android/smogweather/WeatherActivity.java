a310faf9d2b8f86992bc2ff314b042ffbb815e06
rhonea yuen <rhonea yuen>
2017-03-28 00:45:06 +0800

    finish the main gui

--- app/src/main/java/com/example/android/smogweather/WeatherActivity.java
+++ app/src/main/java/com/example/android/smogweather/WeatherActivity.java
@@ -1,13 +1,147 @@
 package com.example.android.smogweather;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.smogweather.gson.Forecast;
+import com.example.android.smogweather.gson.Weather;
+import com.example.android.smogweather.util.HttpUtil;
+import com.example.android.smogweather.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private ScrollView mWeatherLayout;
+
+    private TextView mTitleCity;
+
+    private TextView mTitleUpdateTime;
+
+    private TextView mDegreeText;
+
+    private TextView mWeatherInfoText;
+
+    private LinearLayout mForecastLayout;
+
+    private TextView mAQIText;
+
+    private TextView mPM25Text;
+
+    private TextView mComfortText;
+
+    private TextView mCarWashText;
+
+    private TextView mSportText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        //initialize
+        mWeatherLayout = (ScrollView) findViewById(R.id.weather_layout);
+        mTitleCity = (TextView) findViewById(R.id.title_city);
+        mTitleUpdateTime = (TextView) findViewById(R.id.title_update_time);
+        mDegreeText = (TextView) findViewById(R.id.degree_text);
+        mWeatherInfoText = (TextView) findViewById(R.id.weather_info_text);
+        mForecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
+        mAQIText = (TextView) findViewById(R.id.aqi_text);
+        mPM25Text = (TextView) findViewById(R.id.pm25_text);
+        mComfortText = (TextView) findViewById(R.id.comfort_text);
+        mCarWashText = (TextView) findViewById(R.id.car_wash_content);
+        mSportText = (TextView) findViewById(R.id.sport_text);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather", null);
+        if (weatherString != null) {
+            //exist buffer
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        } else {
+            String weatherId = getIntent().getStringExtra("weather_id");
+            mWeatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
+        }
+    }
+
+    private void requestWeather(final String weatherId) {
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WeatherActivity.this, "Failed to request information of weather", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (weather != null && "ok".equals(weather.status)) {
+                            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                            editor.putString("weather", responseText);
+                            editor.apply();
+                            showWeatherInfo(weather);
+                        } else {
+                            Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void showWeatherInfo(Weather weather) {
+        String cityName = weather.mBasic.cityName;
+        String updateTime = weather.mBasic.mUpdate.updateTime.split(" ")[1];
+        String degree = weather.mNow.temperature + "℃";
+        String weatherInfo = weather.mNow.more.info;
+        mTitleCity.setText(degree);
+        mTitleUpdateTime.setText(updateTime);
+        mWeatherInfoText.setText(weatherInfo);
+        mForecastLayout.removeAllViews();
+        for (Forecast forecast : weather.mForecastList) {
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, mForecastLayout, false);
+            TextView dateText = (TextView) view.findViewById(R.id.date_text);
+            TextView infoText = (TextView) view.findViewById(R.id.info_text);
+            TextView maxText = (TextView) view.findViewById(R.id.max_text);
+            TextView minText = (TextView) view.findViewById(R.id.min_text);
+            dateText.setText(forecast.date);
+            infoText.setText(forecast.mMore.info);
+            maxText.setText(forecast.mTemperature.max);
+            minText.setText(forecast.mTemperature.min);
+            mForecastLayout.addView(view);
+        }
+        if (weather.mAQI != null) {
+            mAQIText.setText(weather.mAQI.mCity.aqi);
+            mPM25Text.setText(weather.mAQI.mCity.pm25);
+        }
+        String comfort = "舒适度" + weather.mSuggestion.mComfort.info;
+        String carWash = "洗车指数" + weather.mSuggestion.mCarWash.info;
+        String sport = "运动建议" + weather.mSuggestion.mSport.info;
+        mComfortText.setText(comfort);
+        mCarWashText.setText(carWash);
+        mSportText.setText(sport);
+        mWeatherLayout.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/example/android/smogweather/gson/Suggestion.java
+++ app/src/main/java/com/example/android/smogweather/gson/Suggestion.java
@@ -17,21 +17,21 @@ public class Suggestion {
     public Sport mSport;
 
 
-    private class Comfort {
+    public class Comfort {
 
         @SerializedName("txt")
         public String info;
 
     }
 
-    private class CarWash {
+    public class CarWash {
 
         @SerializedName("txt")
         public String info;
 
     }
 
-    private class Sport {
+    public class Sport {
 
         @SerializedName("txt")
         public String info;
